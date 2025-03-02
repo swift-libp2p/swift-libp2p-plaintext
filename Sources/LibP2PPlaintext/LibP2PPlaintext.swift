71b61d575826c0f17c2d3723e0237f34ddc12151
Brandon <32753167+btoms20@users.noreply.github.com>
2025-03-01 21:58:37 -0800

    Updated license header

--- Sources/LibP2PPlaintext/LibP2PPlaintext.swift
+++ Sources/LibP2PPlaintext/LibP2PPlaintext.swift
@@ -1,5 +1,18 @@
-import LibP2P
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libp2p open source project
+//
+// Copyright (c) 2022-2025 swift-libp2p project authors
+// Licensed under MIT
+//
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of swift-libp2p project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
 
+import LibP2P
 
 public enum PlaintextErrors:Error {
     case invalidPeerIDExchange
